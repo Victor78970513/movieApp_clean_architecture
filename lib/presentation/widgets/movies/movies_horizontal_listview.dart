041979b87d78eb76060353adccc73045e977d0a4
Victor78970513 <yer29.chok@gmail.com>
2023-08-11 23:05:37 -0400

    feat: humanformats added

--- lib/presentation/widgets/movies/movies_horizontal_listview.dart
+++ lib/presentation/widgets/movies/movies_horizontal_listview.dart
@@ -1,3 +1,5 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/config/helpers/human_formats.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
@@ -42,6 +44,7 @@ class _Slide extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textStyles = Theme.of(context).textTheme;
     return Container(
       margin: const EdgeInsets.symmetric(horizontal: 8),
       child: Column(
@@ -53,15 +56,55 @@ class _Slide extends StatelessWidget {
               borderRadius: BorderRadius.circular(20),
               child: Image.network(
                 movie.posterPath,
+                fit: BoxFit.cover,
                 width: 150,
                 loadingBuilder: (context, child, loadingProgress) {
                   if (loadingProgress != null) {
-                    return const Center(
-                        child: CircularProgressIndicator(strokeWidth: 2));
+                    // return const SizedBox(
+                    //   height: 150,
+                    //   child: DecoratedBox(
+                    //     decoration: BoxDecoration(color: Colors.black12),
+                    //   ),
+                    // );
+                    return const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Center(
+                          child: CircularProgressIndicator(strokeWidth: 2)),
+                    );
                   }
-                  return child;
+                  return FadeInRight(child: child);
                 },
               ),
+            ),
+          ),
+
+          const SizedBox(height: 5),
+          //* Title
+          SizedBox(
+            width: 150,
+            child: Center(
+              child: Text(
+                movie.title,
+                maxLines: 2,
+                style: textStyles.titleSmall,
+              ),
+            ),
+          ),
+
+          //*Rating
+          SizedBox(
+            width: 130,
+            child: Row(
+              children: [
+                Icon(Icons.star_half_outlined, color: Colors.yellow.shade800),
+                const SizedBox(width: 3),
+                Text('${movie.voteAverage}',
+                    style: textStyles.bodyMedium
+                        ?.copyWith(color: Colors.yellow.shade800)),
+                const Spacer(),
+                Text(HumanFormats.number(movie.popularity),
+                    style: textStyles.bodySmall)
+              ],
             ),
           )
         ],
